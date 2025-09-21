b296cb1431dde07c45123db7b1cdcdbc04c86cb5
Alexander López <lopez.manrique.alexander@gmail.com>
2025-09-21 14:16:16 -0500

    Update setup.py

--- lab2/setup.py
+++ lab2/setup.py
@@ -12,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*'))
         (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
     install_requires=['setuptools'],
