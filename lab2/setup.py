1d6e6a8a88e5dd798078a96d7d23bd3e5a7b1e3a
Alexander López <lopez.manrique.alexander@gmail.com>
2025-09-27 10:40:51 -0500

    Update setup.py

--- lab2/setup.py
+++ lab2/setup.py
@@ -13,6 +13,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*'))
     ],
     install_requires=['setuptools'],
