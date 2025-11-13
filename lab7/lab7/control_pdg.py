7743c1130db2c5000fcb957ff5e8db206f52f570
Alexander López <lopez.manrique.alexander@gmail.com>
2025-11-13 18:21:12 -0500

    Update control_pdg.py

--- lab7/lab7/control_pdg.py
+++ lab7/lab7/control_pdg.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-
-import rospy
+import rclpy
+import threading
+import numpy as np
 from sensor_msgs.msg import JointState
-from markers import *
+#from markers import *
 from functions import *
-from roslib import packages
 
 import rbdl
 
 if __name__ == '__main__':
 
-  rospy.init_node("control_pdg")
-  pub = rospy.Publisher('joint_states', JointState, queue_size=1)
+  rclpy.init()
+  node = rclpy.create_node('control_pdg')
+  pub = node.create_publisher(JointState, 'joint_states', 10)
   
-  bmarker_actual  = BallMarker(color['RED'])
-  bmarker_deseado = BallMarker(color['GREEN'])
+  #bmarker_actual  = BallMarker(color['RED'])
+  #bmarker_deseado = BallMarker(color['GREEN'])
   
   # Archivos donde se almacenara los datos
   fqact = open("/home/<user>/qactual.txt", "w")
@@ -28,7 +29,7 @@ if __name__ == '__main__':
   # Objeto (mensaje) de tipo JointState
   jstate = JointState()
   # Valores del mensaje
-  jstate.header.stamp = rospy.Time.now()
+  jstate.header.stamp = node.get_clock().now().to_msg()
   jstate.name = jnames
   
   # =============================================================
@@ -43,7 +44,7 @@ if __name__ == '__main__':
   # Posicion resultante de la configuracion articular deseada
   xdes = ur5_fkine(qdes)[0:3,3]
   # Copiar la configuracion articular en el mensaje a ser publicado
-  jstate.position = q
+  jstate.position = q.tolist()
   pub.publish(jstate)
   
   # Definir metodos de Pinocchio para el UR5
@@ -93,10 +94,10 @@ if __name__ == '__main__':
     robot.send_command(u)
 
     # Publicacion del mensaje
-    jstate.position = q
+    jstate.position = q.tolist()
     pub.publish(jstate)
-    bmarker_deseado.xyz(xdes)
-    bmarker_actual.xyz(x)
+    #bmarker_deseado.xyz(xdes)
+    #bmarker_actual.xyz(x)
     t = t+dt
     # Esperar hasta la siguiente  iteracion
     rate.sleep()
